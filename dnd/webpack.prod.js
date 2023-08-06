d7843d6dc2a4c5ab5600c13181c4e2bfd0d6e3f4
Alina <Alin.k4@yandex.ru>
2023-08-06 12:22:30 +0300

    first commit ads

--- dnd/webpack.prod.js
+++ dnd/webpack.prod.js
@@ -1,21 +1,12 @@
-const merge = require('webpack-merge');
-
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
-const TerserPlugin = require('terser-webpack-plugin');
-
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-
-// const path = require('path');
-// const HtmlWebPackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimizer: [
-      new TerserPlugin({}),
-      new OptimizeCSSAssetsPlugin({}),
+      new CssMinimizerPlugin(),
     ],
   },
 });
