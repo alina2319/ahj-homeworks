4e60b1f6f0f9a5e7628e9987f69c65d3c54981f1
Alina <Alin.k4@yandex.ru>
2023-08-07 18:16:55 +0300

    first commit ads

--- http/src/js/app.js
+++ http/src/js/app.js
@@ -1,100 +1,37 @@
-import getTicket from "./functionGetTicket";
-import getRemoveTicketWidget from "./removeTicketWidget";
-import getAddTicketWidget from "./addTicketWidget";
-import getEditTicketWidget from "./editTicketWidget";
-import changeTicketStatus from "./functionChangeTicketStatus";
-import showTicketDescription from "./functionShowTicketDescription";
+import tooltip from "./popover";
 
-// const port = 7070;// for devserver  work
-// const serverUrl = `http://localhost:${port}`;// for devserver  work
-const serverUrl = "https://ahj-hw-http.herokuapp.com/";
+const container = document.querySelector(".container");
 
-const mainContainer = document.querySelector(".container");
-const ticketsContainer = document.querySelector(".tickets-container");
-const addTicketButton = document.querySelector(".add-ticket-button");
+const data = {
+  "btn-1": {
+    title: "Popover first",
+    message: "Message-1",
+  },
+  "btn-2": {
+    title: "Popover second",
+    message: "Message-2",
+  },
+  "btn-3": {
+    title: "Popover third",
+    message: "Message-3",
+  },
+  "btn-4": {
+    title: "Popover fourth",
+    message: "Message-4",
+  },
+};
 
-//  page onload request loading tickets from server
-document.addEventListener("DOMContentLoaded", () => {
-  // console.log('DOMContentLoaded');
-  const xhrLoadTickets = new XMLHttpRequest();
-  xhrLoadTickets.open("GET", `${serverUrl}/?method=allTickets`);
-  xhrLoadTickets.responseType = "json";
-  xhrLoadTickets.addEventListener("load", () => {
-    if (xhrLoadTickets.status >= 200 && xhrLoadTickets.status < 300) {
-      try {
-        // console.log('load from server is ok');
-        const responsedTickets = xhrLoadTickets.response;
-        // console.log('xhrLoadTickets.response: ', xhrLoadTickets.response);
-        if (!responsedTickets.length) return;
-        responsedTickets.forEach((ticket) => {
-          // console.log(ticket);
-          getTicket(ticket, ticketsContainer);
+container.addEventListener("click", (event) => {
+  event.preventDefault();
+  const { target } = event;
+  const btn = target.closest(".btn");
 
-          const currentTicket = ticketsContainer.lastElementChild;
-          const ticketStatus = currentTicket.querySelector(".ticket-status");
-          const ticketStatusCheckbox = ticketStatus.querySelector(
-            ".ticket-status-checkbox"
-          );
-          if (ticketStatus.dataset.status === "true")
-            ticketStatusCheckbox.classList.remove("hidden");
-          const ticketName = currentTicket.querySelector(".ticket-name");
-          const ticketEdit = currentTicket.querySelector(".ticket-edit-button");
-          const ticketRemove = currentTicket.querySelector(
-            ".ticket-remove-button"
-          );
-
-          //  CHANGE TICKET STATUS
-
-          ticketStatus.addEventListener("click", () => {
-            // console.log('ticketStatus.dataset.status: ', ticketStatus.dataset.status);
-            changeTicketStatus(
-              mainContainer,
-              currentTicket,
-              ticketStatus,
-              ticketStatusCheckbox,
-              serverUrl
-            );
-          });
-
-          //  SHOW DESCRIPTION
-
-          ticketName.addEventListener("click", () => {
-            showTicketDescription(
-              mainContainer,
-              currentTicket,
-              ticketName,
-              serverUrl
-            );
-          });
-
-          //  TICKET EDITING
-
-          ticketEdit.addEventListener("click", () => {
-            //  pop-up modal window
-            getEditTicketWidget(
-              mainContainer,
-              currentTicket,
-              ticketEdit,
-              serverUrl
-            );
-          });
-
-          //  TICKET REMOVING
-
-          ticketRemove.addEventListener("click", () => {
-            getRemoveTicketWidget(mainContainer, currentTicket, serverUrl);
-          });
-        }); // forEach endline
-      } catch (e) {
-        console.error(e);
-        // throw e;
-      }
+  if (btn) {
+    let popover = btn.querySelector(".popover");
+    if (popover) {
+      popover.classList.toggle("hidden");
+    } else {
+      popover = tooltip(data[btn.id], btn);
     }
-  }); // END OF TICKET LOADING
-  xhrLoadTickets.send();
-
-  //  ADD TICKET BUTTON LOGIC
-  addTicketButton.addEventListener("click", () => {
-    getAddTicketWidget(mainContainer, serverUrl);
-  });
-}); // DOMload endline
+  }
+});
